492ea855fdaa0472a363e5f34f39cddd0dad13fe
ibuyas khan <ibuyas.khan77@gmail.com>
2020-05-07 12:28:16 +0200

    Style and asset file

---
+++ src/screens/tabs/style.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var React = require('react-native');
+
+var {
+  StyleSheet,
+} = React;
+
+module.exports = StyleSheet.create({
+
+alwaysred: {
+    backgroundColor: 'red',
+    height: 100,
+    width: 100,
+},
+
+});
